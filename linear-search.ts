b4128691141c6ef4bd0f4e8383ab2d5f2f4764fb
Vladislav Khvostov <vladkhvo@gmail.com>
2017-08-07 23:39:44 +0300

    Update linear-search.ts

--- linear-search.ts
+++ linear-search.ts
@@ -1,1 +1,13 @@
+interface ReduceArrayFunction extends Function {
+    (prev: number, current: number, index: number): number;
+}
+
+function mapNumberWithArrayItem(item: number): ReduceArrayFunction  {
+	return (prev: number, current: number, index): number => current === item ? index : prev;
+}
+
+// O(n)
+function binarySearch(list: number[], item: number): number | boolean {
+	return list.reduce(mapNumberWithArrayItem(item), -1)
+}
 

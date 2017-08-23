de6182a87b31abd6e6a728133263b0f349dde968
Vladislav Khvostov <vladkhvo@gmail.com>
2017-08-23 23:20:20 +0300

    Update binary-search.js

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,25 +1,23 @@
 const binarySearch = (arraySortedNumbers, elementToSearch) => {
-  
-      let leftBoundary = 0;
-      let rightBoundary = arraySortedNumbers.length - 1;
-    
-  
-      while (leftBoundary <= rightBoundary) {
-          let mid = Math.floor((leftBoundary + rightBoundary) / 2);
-  
-        if(arraySortedNumbers[mid] === elementToSearch) {
-          return mid
-        }
-    
-        if(arraySortedNumbers[mid] < elementToSearch) {
-          leftBoundary = mid + 1;
-        }
-    
-        if(arraySortedNumbers[mid] > elementToSearch) {
-          rightBoundary = mid - 1;
-        }
-      }
-      return -1;
+  let leftBoundary = 0;
+  let rightBoundary = arraySortedNumbers.length - 1;
+
+  while (leftBoundary <= rightBoundary) {
+    let mid = Math.floor((leftBoundary + rightBoundary) / 2);
+
+    if(arraySortedNumbers[mid] === elementToSearch) {
+      return mid
+    }
+
+    if(arraySortedNumbers[mid] < elementToSearch) {
+      leftBoundary = mid + 1;
+    }
+
+    if(arraySortedNumbers[mid] > elementToSearch) {
+      rightBoundary = mid - 1;
+    }
   }
+  return -1;
+}
   
   module.exports = binarySearch;
